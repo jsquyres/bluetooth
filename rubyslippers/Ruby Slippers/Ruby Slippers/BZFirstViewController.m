ea965b5b6e098d4005a26a420d0340262c863dfa
Jeff Squyres <jsquyres@cisco.com>
2014-01-12 13:05:22 -0500

    Next checkpoint

--- rubyslippers/Ruby Slippers/Ruby Slippers/BZFirstViewController.m
+++ rubyslippers/Ruby Slippers/Ruby Slippers/BZFirstViewController.m
@@ -12,18 +12,45 @@
 
 @end
 
+static BOOL scanning = false;
+
+static int numTimesOn = 0;
+static int numTimesOff = 0;
+
 @implementation BZFirstViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    if (scanning) {
+        // Set the switch to on
+        UISwitch *foo;
+        [foo setOn:YES animated:NO];
+    } else {
+        // Set the switch to off
+    }
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)EnableBLEScanning:(UISwitch *)sender {
+    if ([sender isOn]) {
+        // They switched it on!  Party time!
+        ++numTimesOn;
+        scanning = true;
+    } else {
+        // They switched it off.  Sadness.
+        ++numTimesOff;
+        scanning = false;
+    }
+    
+    
 }
 
 @end
